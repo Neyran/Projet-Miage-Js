5cf053b5af0b38b5dcd6489c0987a59287e58e5d
Yasmiage <44622591+Yasmiage@users.noreply.github.com>
2019-01-05 20:43:08 +0100

    Update star.js

--- JEU/star.js
+++ JEU/star.js
@@ -1,7 +1,8 @@
 //On construit les étoiles
 let etoiles = [];
 let randometoile = [];
-let bigstar = [];
+let bigStar = [];
+let teleStar = [];
 class Etoile { 
   
 //Le constructeur initialise les variables
@@ -44,11 +45,23 @@ ctx.restore();
 }
 
 update() {
+
+      //collision balle et balle2
+    var collA = ballRadius*2 +1;
+    var collX = balle.x - balle2.x;
+    var collY = balle.y - balle2.y;
+    if (collA > Math.sqrt((collX * collX ) + (collY * collY))) {
+        balle2.x = previousPosX2;
+        balle2.y = previousPosY2;
+        balle.y = previousPosY;
+        balle.x = previousPosX;
+    }
+
     for(var c=0; c<brickColumnCount; c++) {
         for(var r=0; r<brickRowCount; r++) {
               var b = bricks[c][r];
              
-             //Collision étoiel avec mur 1 et 2
+             //Collision étoile avec mur 1 et 2
 
               var collisionLeft3   = this.x + this.size/2>= b.x;
               var collisionRight3  = b.x + brickWidth >= this.x - this.size/2;
@@ -65,51 +78,51 @@ update() {
 
         }
     }
-
-             for(var t=0; t<randometoile.length;t++) {
-               var et = randometoile[t];
-                  console.log(randometoile.length);
+     for(var v=0; v<randometoile.length;v++) {
+               var et = randometoile[v];
+                  //console.log(randometoile.length);
                    switch (et.color) {
-                         case 'white' :
-                         et.color = '#FFA500'; //orange
+                         case 'violet' :
+                         et.color = 'yellow'; //orange
                          break;
-                         case '#FFA500' :
-                         et.color = '#FF0000'; // rouge
+                         case 'yellow' :
+                         et.color = 'green'; // rouge
                          break;
-                         case '#FF0000' :
-                         et.color = '#FFDAB9'; // beige
+                         case 'green' :
+                         et.color = 'pink'; // beige */
+                         break;
+                         case 'pink' :
+                         et.color = 'red'; // beige */
+                         break;
+                         case 'red' :
+                         et.color = 'grey'; // beige */
+                         break;
+                         case 'black' :
+                         et.color = '#F5F5DC'; // beige */
+                         break;
+                         case '#F5F5DC' :
+                         et.color = '#fafaf0'; // beige */
+                         break;
                          default:        
-                         et.color = 'white';
+                         et.color = 'violet';
                     }
                   }
 
-                  //Spécialisation de l'étoile bigStar
-
-                  for(var y=0 ; y<bigstar.length; y++) {
-                    var testetoile = bigstar[y];
-                    testetoile.size = 20;
-                    testetoile.vx = (Math.random()*2 - 1) ;
-                    testetoile.vy = (Math.random()*2 - 1) ;
-              if (b.status == 1 || b.status == 2 ) {
-                if (collisionLeft3 && collisionRight3 && collisionTop3 && collisionBottom3){
-                 
-                   this.vx *= -1;
-                   this.vy *= -1;
-                }
-              }
+             //Spécialisation de l'étoile bigStar
+                  for(var f=0 ; f<teleStar.length; f++) {
+                    console.log(teleStar.length);
+                    var teletoile = teleStar[f];
+                    teletoile.vx = 0;
+                    teletoile.vy = 0;
                   }
 
+            for(var y=0 ; y<bigStar.length; y++) {
+                    console.log(bigStar.length);
+                    var testetoile = bigStar[y];
+                    testetoile.vx = 0;
+                    testetoile.vy = 0;
+                  }
 
-    //collision balle et balle2
-    var collA = ballRadius*2 +1;
-    var collX = balle.x - balle2.x;
-    var collY = balle.y - balle2.y;
-    if (collA > Math.sqrt((collX * collX ) + (collY * collY))) {
-        balle2.x = previousPosX2;
-        balle2.y = previousPosY2;
-        balle.y = previousPosY;
-        balle.x = previousPosX;
-    }
 
     //Collision balle etoile
     var collball = this.x + this.size/2>= balle.x - ballRadius;
@@ -123,28 +136,133 @@ update() {
     var collball23 = this.y + this.size/2>= balle2.y - ballRadius;
     var collball24 = balle2.y + ballRadius >= this.y - this.size/2;
 
-
-
     //Nouvelle localisation de la balle en fonction de la couleur de l'étoile qu'elle touche
     switch (this.color) {
-        case 'white' :
+          case 'violet' :
+          if(collball02 && collball22 && collball23 && collball24) { 
+            balle2.x = 500; 
+            balle2.y = 500;
+
+          }
+          if((collball && collball2 && collball3 && collball4)){ 
+          balle.x = 500;
+          balle.y = 500;
+          }
+          break;
+       case 'yellow' :
           if(collball02 && collball22 && collball23 && collball24) {
-            balle2.x = lc/2+15; //changer les localisations en fonctions de couleurs
-            balle2.y = hc/2;
-            this.size =20;
-            this.color ='#FFDAB9'; //change la couleur de l'étoile touchée ici beige
-            balle2.rad *=1.5;
-            vit = 2;
-            console.log(vit);
-
-
+            this.size=0;
+            score2 +=2;
+            this.x = 0;
+            this.y = 0;
           }
           if((collball && collball2 && collball3 && collball4)){
-          balle.x = lc/2+15;
-          balle.y = hc/2;
-          this.color = '#FFDAB9'; // beige
+            this.size=0;
+            score1 +=2;
+            this.x = 0;
+            this.y = 0;
           }
-        break;
+           break;
+         case 'green' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+            vit2+= 4;
+          }
+          if((collball && collball2 && collball3 && collball4)){
+          vit1 += 4;
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+
+          }
+          break;
+          case 'pink' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+            vit2 -= 4;
+          }
+          if((collball && collball2 && collball3 && collball4)){
+          vit1 -= 4;
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+
+          }
+          break;
+          case 'red' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+            score2 =0;
+          }
+          if((collball && collball2 && collball3 && collball4)){
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+            score1 =0;
+
+          }
+          break;
+          case 'black' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            balle2.rad =5; 
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+           
+          }
+          if((collball && collball2 && collball3 && collball4)){
+            balle.rad *=5;
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+           
+          }
+          break;
+
+          case 'grey' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            balle2.rad *=1.5; 
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+          
+          }
+          if((collball && collball2 && collball3 && collball4)){
+            balle.rad *=1.5;
+            this.x = 0;
+            this.y = 0;
+            this.size=0;
+           
+          }
+          break;
+          case 'white' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            balle2.x = 1100; //changer les localisations en fonctions de couleurs
+            balle2.y = 200;
+          }
+          if((collball && collball2 && collball3 && collball4)){
+          balle.x = 1100;
+          balle.y = 200;
+
+          }
+          break;
+         case 'orange' :
+          if(collball02 && collball22 && collball23 && collball24) {
+            balle2.x = 1650; //changer les localisations en fonctions de couleurs
+            balle2.y = 290;
+          }
+          if((collball && collball2 && collball3 && collball4)){
+          balle.x = 1650;
+          balle.y = 290;
+
+          }
+          break;
         default:        
           if(collball02 && collball22 && collball23 && collball24) {
             balle2.x = lc/2+15;
@@ -155,14 +273,16 @@ update() {
           balle.x = lc/2-15;
           balle.y = hc/2;
           }
+          break;
     }
 
+/*
     //collision canvas
     if (this.x < 0 + this.size/2 || this.x > canvas.width - this.size/2)
     this.vx *= -1;
     if (this.y < 0 + this.size/2 || this.y > canvas.height - this.size/2)
     this.vy *= -1;
-
+*/
     //vitesse étoile
     this.x += this.vx;
     this.y += this.vy;
